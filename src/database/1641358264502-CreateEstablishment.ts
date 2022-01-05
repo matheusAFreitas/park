9c8905e32b185b3f2f0876c7e1c0f18eb4763ea3
matheusAFreitas <matheusalvesdefreitas@gmail.com>
2022-01-05 02:01:07 -0300

    establishments migration created and working

---
+++ src/database/1641358264502-CreateEstablishment.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateEstablishment1641358264502 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'establishments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'CNPJ',
+            type: 'varchar',
+          },
+          {
+            name: 'endereço',
+            type: 'varchar',
+          },
+          {
+            name: 'telefone',
+            type: 'varchar',
+          },
+          {
+            name: 'qtdMoto',
+            type: 'int',
+          },
+          {
+            name: 'qtdCarro',
+            type: 'int',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('establishments');
+    await queryRunner.query('DROP EXTENSION IF EXISTS "uuid-ossp"');
+  }
+}
